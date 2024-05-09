78062758e19ea081d1fbeacc9b866469a256784a
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-05-09 11:19:30 +1000

    Added GRF-based covariates

--- Simulate_Covariates.R
+++ Simulate_Covariates.R
@@ -20,6 +20,7 @@ library(spatstat)
 library(ggplot2)
 library(dplyr)
 library(ggpubr)
+library(viridis)
 
 set.seed(42)
 
@@ -51,11 +52,6 @@ gridcov1 <- outer(y0,x0, function (x,y) multiplier*x + 0*y)
 
 gridcov2 <- outer(y0,x0, function (x,y) multiplier*y + 0*x)
 
-# Plot the covariates
-image.plot(list(x=x0, y=y0, z=t(gridcov1)), main='Covariate 1', asp=1) 
-image.plot(list(x=x0, y=y0, z=t(gridcov2)), main='Covariate 2', asp=1) 
-
-library(viridis)
 
 c1 <- gridcov1 %>% 
   reshape2::melt(c("x", "y"), value.name = "cov") %>% 
@@ -96,7 +92,8 @@ ggarrange(c1, c2)
 # cov1 <- grf(n = ncell(r), grid = "reg", nx = 300, ny = 300, xlims = c(0,300), ylims = c(0,300), cov.model = "matern", cov.pars=c(50,0.1), kappa = 0.5, nugget = 0)
 
 
-#Following the approach of Fletcher Jr. et al. (2023) and Grimmett et al. (2021), we use the NLMR package.
+# Following the approach of Fletcher Jr. et al. (2023) and Grimmett et al. (2021), we use the NLMR package.
+# Create simulated habitat using Gaussian random fields within a 100 x 100 grid
 
 
 # First install NLMR
@@ -105,7 +102,58 @@ ggarrange(c1, c2)
 
 library(NLMR)
 library(landscapetools)
+library(RandomFields) # Note that RandomFields is no longer on CRAN. Downloaded archived file.
+library(terra)
+
+cov1 <- nlm_gaussianfield(ncol = 300,
+                          nrow = 300,
+                          resolution = 1,
+                          autocorr_range = 50, # Maximum range (raster units) of spatial autocorrelation
+                          mag_var = 1, # Magnitude of variation over the landscape
+                          nug = 0.01, # Magnitude of variation in the scale of the autocorr_range (smaller values = more homogenous)
+                          mean = 0.5, # Mean value over the field
+                          user_seed = 2L, # Set random seed for the simulation
+                          rescale = T # If T, the values are rescaled between 0 and 1
+                          ) %>% 
+  rast()
+
+cov2 <- nlm_gaussianfield(ncol = 300,
+                          nrow = 300,
+                          resolution = 1,
+                          autocorr_range = 50, # Maximum range (raster units) of spatial autocorrelation
+                          mag_var = 1, # Magnitude of variation over the landscape
+                          nug = 0.01, # Magnitude of variation in the scale of the autocorr_range (smaller values = more homogenous)
+                          mean = 0.5, # Mean value over the field
+                          user_seed = 3L, # Set random seed for the simulation
+                          rescale = T # If T, the values are rescaled between 0 and 1
+) %>% 
+  rast()
 
 
+c1 <- cov1 %>% 
+  as.data.frame(xy = T) %>%  
+  ggplot() + 
+  geom_tile(aes(x = x, y = y, fill = layer)) + 
+  scale_fill_viridis() +
+  coord_fixed() + 
+  theme_bw() + 
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.ticks = element_blank(),
+        legend.title = element_blank()) +
+  ggtitle('Covariate 1')
 
+c2 <- cov2 %>% 
+  as.data.frame(xy = T) %>%  
+  ggplot() + 
+  geom_tile(aes(x = x, y = y, fill = layer)) + 
+  scale_fill_viridis() +
+  coord_fixed() + 
+  theme_bw() + 
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.ticks = element_blank(),
+        legend.title = element_blank()) +
+  ggtitle('Covariate 2')
 
+ggarrange(c1, c2)
