76178ded31e593f6c2a850dc238617c7db387722
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-05-23 08:45:45 +1000

    Case study setup

--- Case_Study/Data_Setup_Case_Study.R
+++ Case_Study/Data_Setup_Case_Study.R
@@ -1,1 +1,20 @@
 
+library(terra)
+library(ggplot2)
+library(tidyverse)
+library(viridis)
+
+
+# Load ice-free land ------------------------------------------------------
+
+ice_free <- st_read(here("Data/add_rock_outcrop_medium_res_polygon_v7.3.shp"), crs = 3031)
+
+
+# Load habitat covariates -------------------------------------------------
+
+
+
+
+# Calculate topographic wetness index -------------------------------------
+
+
