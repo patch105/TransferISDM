49331435eea922de08f134dc142c66af636cc4ef
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-06-17 08:21:43 +1000

    Initialise prediction code

---
+++ Predict_from_fitted.R
@@ -0,0 +1,52 @@
+
+#### PREDICT for every extrap type, for every rep, for every model type
+# Save prediction to the model list
+####
+
+# Get the names of the extrap types for indexing
+extrap_names <- names(extrap.reps.out.mods)
+
+# For every level of extrap type (Low, Mod, High)
+for(extrap.type in seq_along(extrap.reps.out.mods)) {
+  
+  # Extract the name ("Low") for indexing from the names list
+  name <- extrap_names[extrap.type] 
+  
+  # For every replicate
+  for(rep in seq_along(extrap.type)) {
+    
+    # Extract the models dataframe [[name]] double brackets for list extract
+    models_df <- extrap.reps.out.mods[[name]][[rep]]$models
+    
+    for (i in 1:2) { # NEED TO ADD BACK IN nrow(models_df) ONCE HAVE PA WORKING
+      
+      mod <- models_df[i, "Model"][[i]]
+      
+      
+      
+      mod$preds <- predict(mod,
+                           covars = cov,
+                           S = 500, 
+                           intercept.terms = "PO_Intercept",
+                           type = "link")
+      
+      
+      # Save the updated model back to the dataframe
+      models_df[i, "Model"][[1]] <- mod
+      
+    }
+    
+    # Save the updated models dataframe back to the original list
+    extrap.reps.out.mods[[name]][[rep]]$models <- models_df
+    
+    
+  }
+  
+  }
+
+
+####### TO DO: 
+# Get code working for running through all the reps
+# Check that the saving mods$preds back into the list works
+# CHECK TYPE = "link" function
+
