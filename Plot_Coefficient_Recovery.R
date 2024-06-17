4caaa603d5f3b50f52a310a5888c30d3be0564e6
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-06-18 09:42:12 +1000

    Add plot mean width CI and CI contains true betas

--- Plot_Coefficient_Recovery.R
+++ Plot_Coefficient_Recovery.R
@@ -46,3 +46,53 @@ beta_plot
 ggsave(plot = beta_plot, filename = paste0("output/Extrap_10_rep_PO_ISDM_beta_plot.png"), w = 21.5, h = 15, units = "cm", dpi = 400, device = "png")
 
 
+#### Plot mean width of the credible interval #################
+
+extrap.scenario.df.CI <- extrap.scenario.df %>% 
+  mutate(beta1.cred.int = beta1_975 - beta1_25,
+         beta2.cred.int = beta2_975 - beta2_25,
+         beta1.cred.int.true = ifelse(beta1 >= beta1_25 &  beta1 <= beta1_975, 1, 0),
+         beta2.cred.int.true = ifelse(beta2 >= beta2_25 & beta2 <= beta2_975, 1, 0))
+
+b1.CI.width <- extrap.scenario.df.CI %>% 
+  ggplot(aes(x = extrap.type, y = beta1.cred.int, fill = mod.type)) +
+  geom_boxplot() +
+  labs(x = "Extrapolation", y = bquote(beta[1] ~ " Credible Interval Width"), fill = "Model Type") +
+  scale_x_discrete(labels = c("Low", "Mod", "High")) +
+  scale_fill_manual(values = c("Integrated" = "purple", "PO" = "skyblue")) +
+  theme_bw()
+
+b2.CI.width <- extrap.scenario.df.CI %>% 
+  ggplot(aes(x = extrap.type, y = beta2.cred.int, fill = mod.type)) +
+  geom_boxplot() +
+  labs(x = "Extrapolation", y = bquote(beta[2] ~ " Credible Interval Width"), fill = "Model Type") +
+  scale_x_discrete(labels = c("Low", "Mod", "High")) +
+  scale_fill_manual(values = c("Integrated" = "purple", "PO" = "skyblue")) +
+  theme_bw()
+
+beta_CI_width_plot <- ggarrange(b1.CI.width , b2.CI.width, common.legend = T,  ncol = 2, nrow = 1)
+
+beta_CI_width_plot
+
+ggsave(plot = beta_plot, filename = paste0("output/Extrap_10_rep_PO_ISDM_beta_CI_width_plot.png"), w = 21.5, h = 15, units = "cm", dpi = 400, device = "png")
+
+
+# Plot whether credible interval contains true betas ----------------------
+
+b1.CI.true <- extrap.scenario.df.CI %>% 
+  ggplot(aes(x = extrap.type, y = beta1.cred.int.true, fill = mod.type)) +
+  geom_boxplot() +
+  labs(x = "Extrapolation", y = "Beta 1 Credible Interval Contains True Beta", fill = "Model Type") +
+  scale_x_discrete(labels = c("Low", "Mod", "High")) +
+  scale_fill_manual(values = c("Integrated" = "purple", "PO" = "skyblue")) +
+  theme_bw()
+
+b2.CI.true <- extrap.scenario.df.CI %>%
+  ggplot(aes(x = extrap.type, y = beta2.cred.int.true, fill = mod.type)) +
+  geom_boxplot() +
+  labs(x = "Extrapolation", y = "Beta 2 Credible Interval Contains True Beta", fill = "Model Type") +
+  scale_x_discrete(labels = c("Low", "Mod", "High")) +
+  scale_fill_manual(values = c("Integrated" = "purple", "PO" = "skyblue")) +
+  theme_bw()
+
+
