f964801c94735cf96b939f9c32a4adbcbe2d6af5
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-06-17 17:17:15 +1000

    Rename plotting code

--- Plot_Coefficient_Recovery.R
+++ Plot_Coefficient_Recovery.R
@@ -14,28 +14,33 @@ extrap.scenario.df <- data.frame(
   beta2_975 = c(0.09, 0.04, 0.095, 0.03, 0.1, 0.05, 0.105, 0.035, 0.11, 0.045, 0.115, 0.04) * 0.975
 )
 
-
-beta0 <- 5 # Intercept
-beta1 <- 0.5 # Coefficient for cov 1
-beta2 <- 0.1 # Coefficient for cov 2
+# 
+# beta0 <- 5 # Intercept
+# beta1 <- 0.5 # Coefficient for cov 1
+# beta2 <- 0.1 # Coefficient for cov 2
 
 # Plot the coefficients
 extrap.scenario.df %>% 
+  ggplot(aes(x = extrap.type, y = beta1, fill = mod.type))+
+  geom_boxplot() +
+  theme_bw()
+
   ggplot(aes(x = extrap.type, y = beta1)) +
   geom_point() +
   theme_bw()
 
 
-b1 <- ggplot(extrap.scenario.df, aes(x = extrap.type, y = beta1, fill = mod.type)) +
+b1 <- extrap.scenario.df %>% 
+  ggplot(aes(x = extrap.type, y = beta1, fill = mod.type)) +
   geom_boxplot() +
   geom_hline(yintercept = beta1, linetype = "dashed", color = "red") +
-  geom_errorbar(aes(ymin = beta1_25, ymax = beta1_975), width = 0.2, position = position_dodge(0.9)) +
   labs(x = "Extrapolation", y = expression(beta[1]), fill = "Model Type") +
   scale_x_discrete(labels = c("Low", "Mod", "High")) +
   scale_fill_manual(values = c("Integrated" = "purple", "PO" = "skyblue")) +
   theme_bw()
 
-b2 <- ggplot(extrap.scenario.df, aes(x = extrap.type, y = beta2, fill = mod.type)) +
+b2 <- extrap.scenario.df %>% 
+  ggplot(aes(x = extrap.type, y = beta2, fill = mod.type)) +
   geom_boxplot() +
   geom_hline(yintercept = beta2, linetype = "dashed", color = "red") +
   labs(x = "Extrapolation", y = expression(beta[2]), fill = "Model Type") +
@@ -44,6 +49,8 @@ b2 <- ggplot(extrap.scenario.df, aes(x = extrap.type, y = beta2, fill = mod.type
   theme_bw()
 
 beta_plot <- ggarrange(b1 , b2, common.legend = T,  ncol = 2, nrow = 1)
+
+beta_plot
 
 ggsave(plot = beta_plot, filename = paste0("output/mock_beta_plot.png"), w = 21.5, h = 15, units = "cm", dpi = 400, device = "png")
 
