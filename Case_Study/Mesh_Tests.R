1e488bb386d16acc54e2e431a9075bd12830fbc8
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-06-28 13:34:16 +1000

    Trying different buffer widths

--- Case_Study/Mesh_Tests.R
+++ Case_Study/Mesh_Tests.R
@@ -66,3 +66,73 @@ ggplot() +
   
 
   
+############ 27/06/2024 ###################
+
+# Trying a couple of different buffer widths
+ACBRS.NorthEastAnt.buffer1k <- terra::buffer(ACBRS.NorthEastAnt, 1000)
+ACBRS.NorthEastAnt.buffer1k <- terra::aggregate(ACBRS.NorthEastAnt.buffer1k, by = NULL, dissolve = T, fun = "mean")
+
+ACBRS.NorthEastAnt.buffer200 <- terra::buffer(ACBRS.NorthEastAnt, 200)
+ACBRS.NorthEastAnt.buffer200 <- terra::aggregate(ACBRS.NorthEastAnt.buffer200, by = NULL, dissolve = T, fun = "mean")
+
+plot(ACBRS.NorthEastAnt.buffer1k)
+plot(ACBRS.NorthEastAnt.buffer200)
+
+boundary <- fmesher::fm_as_segm(st_as_sf(ACBRS.NorthEastAnt))
+boundary1k <- fmesher::fm_as_segm(st_as_sf(ACBRS.NorthEastAnt.buffer1k))
+boundary200 <- fmesher::fm_as_segm(st_as_sf(ACBRS.NorthEastAnt.buffer200))
+
+dep.range <- 10000 # 10km Set the range based on biology
+
+mesh.boundary <- fmesher::fm_mesh_2d_inla(loc = st_coordinates(PO.sf),
+                                          boundary = boundary,
+                                          max.edge = c(0.2, 0.5)*dep.range,
+                                          cutoff = 50,
+                                          crs=3031)
+
+mesh.200.boundary <- fmesher::fm_mesh_2d_inla(loc = st_coordinates(PO.sf),
+                                              boundary = boundary200,
+                                              max.edge = c(0.2, 0.5)*dep.range,
+                                              cutoff = 50,
+                                              crs=3031)
+
+mesh.1k.boundary <- fmesher::fm_mesh_2d_inla(loc = st_coordinates(PO.sf),
+                                                      boundary = boundary1k,
+                                                      max.edge = c(0.2, 0.5)*dep.range,
+                                                      cutoff = 50,
+                                                      crs=3031)
+
+ggplot() +
+  inlabru::gg(mesh.boundary)
+
+ggplot() +
+  inlabru::gg(mesh.boundary) +
+  # gg(mesh.vrt, color = "red") +
+  coord_sf(
+    xlim = c(st_bbox(Vestfold.landsat.sf)$xmin, st_bbox(Vestfold.landsat.sf)$xmax), 
+    ylim = c(st_bbox(Vestfold.landsat.sf)$ymin, st_bbox(Vestfold.landsat.sf)$ymax))
+
+
+ggplot() +
+  inlabru::gg(mesh.200.boundary)
+
+ggplot() +
+  inlabru::gg(mesh.200.boundary) +
+  # gg(mesh.vrt, color = "red") +
+  coord_sf(
+    xlim = c(st_bbox(Vestfold.landsat.sf)$xmin, st_bbox(Vestfold.landsat.sf)$xmax), 
+    ylim = c(st_bbox(Vestfold.landsat.sf)$ymin, st_bbox(Vestfold.landsat.sf)$ymax))
+
+
+ggplot() +
+  inlabru::gg(mesh.1k.boundary)
+
+ggplot() +
+  inlabru::gg(mesh.1k.boundary) +
+  # gg(mesh.vrt, color = "red") +
+  coord_sf(
+    xlim = c(st_bbox(Vestfold.landsat.sf)$xmin, st_bbox(Vestfold.landsat.sf)$xmax), 
+    ylim = c(st_bbox(Vestfold.landsat.sf)$ymin, st_bbox(Vestfold.landsat.sf)$ymax))
+
+
+
