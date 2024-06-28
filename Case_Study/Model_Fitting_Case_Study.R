50e591bfc51d097f95a18190a268997483b1562d
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-06-28 13:34:38 +1000

    Updating priors

--- Case_Study/Model_Fitting_Case_Study.R
+++ Case_Study/Model_Fitting_Case_Study.R
@@ -21,15 +21,18 @@ my.control <- list(coord.names = c("x", "y"),
                    prior.mean = 0,
                    int.sd = 1000, # Intercept standard deviation
                    other.sd = 10, # Covariate effect standard deviation
-                   addRandom = FALSE) # No random effect
+                   addRandom = FALSE, # No random effect
+                   standardiseCovariates = FALSE) 
 
 my.control.GRF <- list(coord.names = c("x", "y"),
                        prior.mean = 0,
                        int.sd = 1000, # Intercept standard deviation
                        other.sd = 10, # Covariate effect standard deviation
-                       prior.range = c(10, 0.1), # Prior chance 10% that parameter falls below range of 1km
-                       prior.space.sigma = c(50, 0.1), # Prior chance 10% that parameter falls above SD of 5
-                       addRandom = TRUE) # With random effect
+                       prior.range = c(0.1, 0.1), # Prior chance 10% that parameter falls below range of 100m
+                       prior.space.sigma = c(5, 0.1), # Prior chance 10% that parameter falls above SD of 5
+                       addRandom = TRUE, # With random effect
+                       standardiseCovariates = FALSE) 
+
 
 
 PA_fit <- PA_fit %>% mutate(area = 80)
@@ -53,25 +56,38 @@ PA_val <- PA_val %>% mutate(area = 1000)
 #                          offset = NULL, # Default is dep.range
 #                          doPlot = TRUE)
 
-boundary <- fmesher::fm_as_segm(st_as_sf(ACBRS.NorthEastAnt))
+ACBRS.NorthEastAnt.buffer1k <- terra::buffer(ACBRS.NorthEastAnt, 1000)
+ACBRS.NorthEastAnt.buffer1k <- terra::aggregate(ACBRS.NorthEastAnt.buffer1k, by = NULL, dissolve = T, fun = "mean")
+
+boundary1k <- fmesher::fm_as_segm(st_as_sf(ACBRS.NorthEastAnt.buffer1k))
 
 dep.range <- 10000 # 10km Set the range based on biology
 
-mesh.range.10km.cutoff.50 <- fmesher::fm_mesh_2d_inla(loc = st_coordinates(PO.sf),
-                                 boundary = boundary,
-                                 max.edge = c(0.2, 0.5)*dep.range,
-                                 cutoff = 50,
-                                 crs=3031)
-                      
-p1 <- ggplot() +
-  inlabru::gg(mesh.range.10km.cutoff.50) +
-  # gg(mesh.vrt, color = "red") +
-  coord_sf(
-    xlim = c(st_bbox(Vestfold.landsat.sf)$xmin, st_bbox(Vestfold.landsat.sf)$xmax), 
-    ylim = c(st_bbox(Vestfold.landsat.sf)$ymin, st_bbox(Vestfold.landsat.sf)$ymax))
+mesh.1k.boundary <- fmesher::fm_mesh_2d_inla(loc = st_coordinates(PO.sf),
+                                             boundary = boundary1k,
+                                             max.edge = c(0.2, 0.5)*dep.range,
+                                             cutoff = 50,
+                                             crs=3031)
 
-p2 <- ggplot() +
-  inlabru::gg(mesh.range.10km.cutoff.50)
+# boundary <- fmesher::fm_as_segm(st_as_sf(ACBRS.NorthEastAnt.buffer))
+# 
+# dep.range <- 10000 # 10km Set the range based on biology
+# 
+# mesh.range.10km.cutoff.50 <- fmesher::fm_mesh_2d_inla(loc = st_coordinates(PO.sf),
+#                                  boundary = boundary,
+#                                  max.edge = c(0.2, 0.5)*dep.range,
+#                                  cutoff = 50,
+#                                  crs=3031)
+                      
+# p1 <- ggplot() +
+#   inlabru::gg(mesh.range.10km.cutoff.50) +
+#   # gg(mesh.vrt, color = "red") +
+#   coord_sf(
+#     xlim = c(st_bbox(Vestfold.landsat.sf)$xmin, st_bbox(Vestfold.landsat.sf)$xmax), 
+#     ylim = c(st_bbox(Vestfold.landsat.sf)$ymin, st_bbox(Vestfold.landsat.sf)$ymax))
+# 
+# p2 <- ggplot() +
+#   inlabru::gg(mesh.range.10km.cutoff.50)
 
 # ggsave(plot = p1, filename = here("Case_Study/Figures/mesh_range_10km_cutoff_50.png"), width = 10, height = 10, dpi = 300)
 # ggsave(plot = p2, filename = here("Case_Study/Figures/mesh_range_10km_cutoff_50_ALL.png"), width = 10, height = 10, dpi = 300)
@@ -83,21 +99,21 @@ gc()
 
 # Integrated Model Fitting ------------------------------------------------
 
-m.int.no.GRF <- isdm(observationList = list(POdat = PO,
-                                            PAdat = PA_fit),
-                     covars = East.Ant.covs.stk,
-                     mesh = mesh.range.10km.cutoff.50,
-                     responseNames = c(PO = NULL, PA = "presence"),
-                     sampleAreaNames = c(PO = NULL, PA = "area"),
-                     distributionFormula = ~0 + elev + slope + aspect, # Linear w covs
-                     biasFormula = ~1, #Intercept only
-                     artefactFormulas = list(PA = ~1), # Intercept only
-                     control = my.control)    
+# m.int.no.GRF <- isdm(observationList = list(POdat = PO,
+#                                             PAdat = PA_fit),
+#                      covars = East.Ant.covs.stk,
+#                      mesh = mesh.range.10km.cutoff.50,
+#                      responseNames = c(PO = NULL, PA = "presence"),
+#                      sampleAreaNames = c(PO = NULL, PA = "area"),
+#                      distributionFormula = ~0 + elev + slope + aspect, # Linear w covs
+#                      biasFormula = ~1, #Intercept only
+#                      artefactFormulas = list(PA = ~1), # Intercept only
+#                      control = my.control)    
 
 m.int.GRF <- isdm(observationList = list(POdat = PO,
                                          PAdat = PA_fit),
                   covars = East.Ant.covs.stk,
-                  mesh = mesh.range.10km.cutoff.50,
+                  mesh = mesh.1k.boundary,
                   responseNames = c(PO = NULL, PA = "presence"),
                   sampleAreaNames = c(PO = NULL, PA = "area"),
                   distributionFormula = ~0 + elev + slope + aspect, # Linear w covs
@@ -110,7 +126,7 @@ m.int.GRF <- isdm(observationList = list(POdat = PO,
 
 m.PO.no.GRF <- isdm(observationList = list(POdat = PO), 
              covars = East.Ant.covs.stk,
-             mesh = mesh.range.10km.cutoff.50,
+             mesh = mesh.1k.boundary,
              responseNames = NULL,
              sampleAreaNames = NULL,
              distributionFormula = ~0 + elev + slope + aspect, # Linear w one cov
@@ -118,12 +134,21 @@ m.PO.no.GRF <- isdm(observationList = list(POdat = PO),
              artefactFormulas = NULL,
              control = my.control)
 
+m.PO.GRF <- isdm(observationList = list(POdat = PO), 
+                 covars = East.Ant.covs.stk,
+                 mesh = mesh.1k.boundary,
+                 responseNames = NULL,
+                 sampleAreaNames = NULL,
+                 distributionFormula = ~0 + elev + slope + aspect, # Linear w one cov
+                 biasFormula = ~1, #Intercept only
+                 artefactFormulas = NULL,
+                 control = my.control.GRF)
 
 # Presence-Absence Model Fitting ------------------------------------------
 
 m.PA.no.GRF <- isdm(observationList = list(PAdat = PA_fit), 
              covars = East.Ant.covs.stk,
-             mesh = mesh.range.10km.cutoff.50,
+             mesh = mesh.1k.boundary,
              responseNames = c(PA = "presence"),
              sampleAreaNames = c(PA = "area"),
              distributionFormula = ~0 + elev + slope + aspect, # Linear w one cov
@@ -131,15 +156,26 @@ m.PA.no.GRF <- isdm(observationList = list(PAdat = PA_fit),
              artefactFormulas = list(PA = ~1), # Intercept only
              control = my.control)
 
+m.PA.GRF <- isdm(observationList = list(PAdat = PA_fit),
+                 covars = East.Ant.covs.stk,
+                 mesh = mesh.1k.boundary,
+                 responseNames = c(PA = "presence"),
+                 sampleAreaNames = c(PA = "area"),
+                 distributionFormula = ~0 + elev + slope + aspect, # Linear w one cov
+                 biasFormula = NULL, #Intercept only
+                 artefactFormulas = list(PA = ~1), # Intercept only
+                 control = my.control.GRF)
+
+
+
 # Stack models as a list
 mod.list <- list(integrated.no.GRF = m.int.no.GRF,
                  integrated.GRF = m.int.GRF,
                  PO.no.GRF = m.PO.no.GRF,
                  PO.GRF = m.PO.GRF,
-                 PA.no.GRF = m.PA.no.GRF)
+                 PA.no.GRF = m.PA.no.GRF,
+                 PA.GRF = m.PA.GRF)
 
-mod.list <- list(integrated.no.GRF= m.int.no.GRF,
-                 PO.no.GRF = m.PO.no.GRF,
-                 PA.no.GRF = m.PA.no.GRF)
+
 
 
