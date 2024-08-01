063ec9df45806fb9b17e95f7a1765f7cd0b18b63
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-08-01 09:11:21 +0200

    Simulate covariates on 1000 x 1000 grid

--- 1.Simulate_Covariates_AUG.R
+++ 1.Simulate_Covariates_AUG.R
@@ -5,65 +5,16 @@ library(dplyr)
 library(ggpubr)
 library(viridis)
 
+# Install RandomFields package (required for covariate simulation). No longer on CRAN. 
+# Downloaded archived RandomFields utils and RandomFields with code below, accessed from: https://cran.r-project.org/src/contrib/Archive/RandomFieldsUtils/ and https://cran.r-project.org/src/contrib/Archive/RandomFields/ 
+
+# install.packages("C:/Users/n11222026/OneDrive - Queensland University of Technology/Code/Downloaded_packages/RandomFieldsUtils_1.2.5.tar.gz", repos = NULL, type="source")
+
+# install.packages("C:/Users/n11222026/OneDrive - Queensland University of Technology/Code/Downloaded_packages/RandomFields_3.3.14.tar.gz", repos = NULL, type="source")
+
 set.seed(50)
 
 outpath <- getwd()
-
-# Version 1. Simulate Continuous Covariates ------------------------------------------
-
-# Create a bounded domain on [0, 300] x [0, 300]
-
-dim <- c(300, 300)
-
-win <- owin(c(0,dim[1]), c(0,dim[2])) 
-
-# set number of pixels
-spatstat.options(npixel=c(dim[1],dim[2]))
-
-# Create an artificial covariate
-# create y0 and x0 separately as rectangle
-y0 <- seq(win$yrange[1], win$yrange[2],
-          length=spatstat.options()$npixel[2])
-x0 <- seq(win$xrange[1], win$xrange[2],
-          length=spatstat.options()$npixel[1])
-
-# Covariate is a continuous gradient from the bottom to the top with values from 0 to 1
-multiplier <- 1/dim[2]
-
-gridcov1 <- outer(y0,x0, function (x,y) multiplier*x + 0*y)
-
-gridcov2 <- outer(y0,x0, function (x,y) multiplier*y + 0*x)
-
-
-c1 <- gridcov1 %>% 
-  reshape2::melt(c("x", "y"), value.name = "cov") %>% 
-  ggplot() + 
-  geom_tile(aes(x = x, y = y, fill = cov)) + 
-  scale_fill_viridis() +
-  coord_fixed() + 
-  theme_bw() + 
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        legend.ticks = element_blank(),
-        legend.title = element_blank()) +
-  ggtitle('Covariate 1')
-
-c2 <- gridcov2 %>% 
-  reshape2::melt(c("x", "y"), value.name = "cov") %>% 
-  ggplot() + 
-  geom_tile(aes(x = x, y = y, fill = cov)) + 
-  scale_fill_viridis() +
-  coord_fixed() + 
-  theme_bw() + 
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        legend.ticks = element_blank(),
-        legend.title = element_blank()) +
-  ggtitle('Covariate 2')
-
-plot1 <- ggarrange(c1, c2)
-plot1
-ggsave(plot = plot1, filename = paste0(outpath, "/output/Covariates_cont.png"), w = 21.5, h = 18, units = "cm", dpi = 400, device = "png")
 
 
 # Version 2. Simulate Spatial Covariates - XZ code ---------------------------------
@@ -71,16 +22,16 @@ ggsave(plot = plot1, filename = paste0(outpath, "/output/Covariates_cont.png"), 
 # START with set up of resolution and north/east step length for later Site A and B grid creation.
 
 # Set ncol
-ncol <- 100
-nrow <- 100
+ncol <- 1000
+nrow <- 1000
 res <- 0.01
 
 # Create a bounded domain on [0, 1] x [0, 1]
 
 east_min <- 0
-east_max <- 1
+east_max <- 10
 north_min <- 0
-north_max <- 1
+north_max <- 10
 # dom <- spatstat.geom::owin(c(east_min, east_max), c(north_min, north_max))
 
 # We generate the grid resolution from min, max dimensions and the number of pixels
@@ -121,7 +72,7 @@ colnames(coords) <- c("eastings", "northings")
 
 library(NLMR)
 library(landscapetools)
-library(RandomFields) # Note that RandomFields is no longer on CRAN. Downloaded archived file.
+library(RandomFields) # See above for download from archived files. 
 library(terra)
 
 cov1 <- nlm_gaussianfield(ncol = ncol,
@@ -185,157 +136,6 @@ c2 <- cov2 %>%
 plot2 <- ggarrange(c1, c2)
 plot2 
 ggsave(plot = plot2, filename = paste0(outpath, "/output/Covariates_GRF.png"), w = 21.5, h = 18, units = "cm", dpi = 400, device = "png")
-
-
-# Version 2. Simulate Spatial Covariates ---------------------------------------------
-
-# library(geoR)
-# 
-# # cov.model is the covariance model, cov.pars is the parameters (partial sill, range parameter)
-# # Currently using partial sill and range values from Muff et al., 2019 and default kappa / nugget
-# cov1 <- grf(n = ncell(r), grid = "reg", nx = 300, ny = 300, xlims = c(0,300), ylims = c(0,300), cov.model = "matern", cov.pars=c(50,0.1), kappa = 0.5, nugget = 0)
-
-
-# Following the approach of Fletcher Jr. et al. (2023) and Grimmett et al. (2021), we use the NLMR package.
-# Create simulated habitat using Gaussian random fields within a 100 x 100 grid
-
-
-# First install NLMR
-# remotes::install_github("ropensci/NLMR")
-# install.packages("landscapetools")
-
-library(NLMR)
-library(landscapetools)
-library(RandomFields) # Note that RandomFields is no longer on CRAN. Downloaded archived file.
-library(terra)
-
-cov1 <- nlm_gaussianfield(ncol = 300,
-                          nrow = 300,
-                          resolution = 1,
-                          autocorr_range = 50, # Maximum range (raster units) of spatial autocorrelation
-                          mag_var = 1, # Magnitude of variation over the landscape
-                          nug = 0.01, # Magnitude of variation in the scale of the autocorr_range (smaller values = more homogenous)
-                          mean = 0.5, # Mean value over the field
-                          user_seed = 2L, # Set random seed for the simulation
-                          rescale = T # If T, the values are rescaled between 0 and 1
-                          ) %>% 
-  rast()
-
-cov2 <- nlm_gaussianfield(ncol = 300,
-                          nrow = 300,
-                          resolution = 1,
-                          autocorr_range = 50, # Maximum range (raster units) of spatial autocorrelation
-                          mag_var = 1, # Magnitude of variation over the landscape
-                          nug = 0.01, # Magnitude of variation in the scale of the autocorr_range (smaller values = more homogenous)
-                          mean = 0.5, # Mean value over the field
-                          user_seed = 3L, # Set random seed for the simulation
-                          rescale = T # If T, the values are rescaled between 0 and 1
-) %>% 
-  rast()
-
-
-c1 <- cov1 %>% 
-  as.data.frame(xy = T) %>%  
-  ggplot() + 
-  geom_tile(aes(x = x, y = y, fill = layer)) + 
-  scale_fill_viridis() +
-  coord_fixed() + 
-  theme_bw() + 
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        legend.ticks = element_blank(),
-        legend.title = element_blank()) +
-  ggtitle('Covariate 1')
-
-c2 <- cov2 %>% 
-  as.data.frame(xy = T) %>%  
-  ggplot() + 
-  geom_tile(aes(x = x, y = y, fill = layer)) + 
-  scale_fill_viridis() +
-  coord_fixed() + 
-  theme_bw() + 
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        legend.ticks = element_blank(),
-        legend.title = element_blank()) +
-  ggtitle('Covariate 2')
-
-plot2 <- ggarrange(c1, c2)
-plot2 
-ggsave(plot = plot2, filename = paste0(outpath, "/output/Covariates_GRF.png"), w = 21.5, h = 18, units = "cm", dpi = 400, device = "png")
-
-
-# Version 3. Simulate Gaussian covariates ----------------------------------------------
-
-library(RandomFields)
-library(RandomFieldsUtils)
-library(scales)
-
-# Create a bounded domain on [0, 1] x [0, 1]
-
-east_min <- 0
-east_max <- 1
-north_min <- 0
-north_max <- 1
-
-# We generate the grid resolution from min, max dimensions and the number of pixels
-
-# Set number of pixels (100 x 100)
-n_bau_east <- 100
-n_bau_north <- 100
-# so now we have n_bau_est x n_bau_north grid cells
-
-# Obtain the cell resolution
-bau_east_step <- (east_max - east_min) / n_bau_east
-bau_north_step <- (north_max - north_min) / n_bau_north 
-
-# Generate grid centroid coordinates
-# We do this so that our centroid begins in the centre of a cell (hence, bau_east_step/2))
-
-eastings <- seq(east_min + bau_east_step/2, east_max - bau_east_step/2, by = bau_east_step)
-northings <- seq(north_min + bau_north_step/2, north_max - bau_north_step/2, by = bau_north_step)
-
-coords <- as.matrix(expand.grid(eastings, northings))
-colnames(coords) <- c("eastings", "northings")
-
-# Simulate the Gaussian covariates
-rand.cov1 <- cbind(x = coords[,1],  y = coords[, 2], rnorm(n_bau_east * n_bau_north))
-colnames(rand.cov1) <- c("x", "y", "cov")
-
-rand.cov2 <- cbind(x = coords[,1],  y = coords[, 2], rnorm(n_bau_east * n_bau_north))
-colnames(rand.cov2) <- c("x", "y", "cov")
-
-# Rescale them to 0 to 1
-rand.cov1[, "cov"] <- rescale(rand.cov1[, "cov"], to = c(0, 1))
-rand.cov2[, "cov"] <- rescale(rand.cov2[, "cov"], to = c(0, 1))
-
-c1 <- rand.cov1 %>% 
-  ggplot() + 
-  geom_tile(aes(x = x, y = y, fill = cov)) + 
-  scale_fill_viridis() +
-  coord_fixed() + 
-  theme_bw() + 
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        legend.ticks = element_blank(),
-        legend.title = element_blank()) +
-  ggtitle('Covariate 1')
-
-c2 <- rand.cov2 %>% 
-  ggplot() + 
-  geom_tile(aes(x = x, y = y, fill = cov)) + 
-  scale_fill_viridis() +
-  coord_fixed() + 
-  theme_bw() + 
-  theme(axis.title.x = element_blank(),
-        axis.title.y = element_blank(),
-        legend.ticks = element_blank(),
-        legend.title = element_blank()) +
-  ggtitle('Covariate 2')
-
-plot3 <- ggarrange(c1, c2)
-plot3 
-ggsave(plot = plot3, filename = paste0(outpath, "/output/Covariates_rand.png"), w = 21.5, h = 18, units = "cm", dpi = 400, device = "png")
 
 
 
