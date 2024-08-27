405ceba2cd973bcab6c0cdf1df2412527cc1fa94
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-08-28 09:11:22 +1000

    Initialise parameter setup

---
+++ 0.Parameters.R
@@ -0,0 +1,9 @@
+
+# COVARIATE PARAMETERS
+
+autocorr_range.cov1 = 50 # Maximum range (raster units) of spatial autocorrelation
+mag_var.cov1 = 1 # Magnitude of variation over the landscape
+
+autocorr_range.cov2 = 200 # Maximum range (raster units) of spatial autocorrelation
+# OR autocorr_range.cov2 = autocorr_range.cov1*1.2
+mag_var.cov2 = 1 # Magnitude of variation over the landscape
