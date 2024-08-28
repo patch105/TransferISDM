99935657e97609378945bad414190c43f3481bad
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-08-28 14:44:02 +1000

    Updating Figure 1 to have Sites A and B close up

--- 0.Making_Method_Figures_AUG.R
+++ 0.Making_Method_Figures_AUG.R
@@ -79,7 +79,7 @@ library(terra)
 cov1 <- nlm_gaussianfield(ncol = ncol,
                           nrow = nrow,
                           resolution = res,
-                          autocorr_range = 50, # Maximum range (raster units) of spatial autocorrelation
+                          autocorr_range = 10, # Maximum range (raster units) of spatial autocorrelation
                           mag_var = 1, # Magnitude of variation over the landscape
                           nug = 0.01, # Magnitude of variation in the scale of the autocorr_range (smaller values = more homogenous)
                           mean = 0.5, # Mean value over the field
@@ -95,7 +95,7 @@ names(cov1) <- "cov"
 cov2 <- nlm_gaussianfield(ncol = ncol,
                           nrow = nrow,
                           resolution = res,
-                          autocorr_range = 50, # Maximum range (raster units) of spatial autocorrelation
+                          autocorr_range = 100, # Maximum range (raster units) of spatial autocorrelation
                           mag_var = 1, # Magnitude of variation over the landscape
                           nug = 0.01, # Magnitude of variation in the scale of the autocorr_range (smaller values = more homogenous)
                           mean = 0.5, # Mean value over the field
@@ -112,35 +112,34 @@ c1 <- cov1 %>%
   as.data.frame(xy = T) %>%  
   ggplot() + 
   geom_tile(aes(x = x, y = y, fill = cov)) + 
-  scale_fill_viridis() +
+  scale_fill_viridis(guide = guide_colorbar(barwidth = 0.4)) +
   coord_fixed() + 
   theme_bw() + 
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         legend.ticks = element_blank(),
-        legend.title = element_blank()) +
+        legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5)) +
   ggtitle('Covariate 1')
 
 c2 <- cov2 %>% 
   as.data.frame(xy = T) %>%  
   ggplot() + 
   geom_tile(aes(x = x, y = y, fill = cov)) + 
-  scale_fill_viridis() +
+  scale_fill_viridis(guide = guide_colorbar(barwidth = 0.5)) +
   coord_fixed() + 
   theme_bw() + 
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         legend.ticks = element_blank(),
-        legend.title = element_blank()) +
+        legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5)) +
   ggtitle('Covariate 2')
 
-cov <- c(cov1, cov2)
 
-names(cov) <- c("cov1", "cov2")
-
-Fig.1a <- ggarrange(c1, c2)
+Fig.1a <- ggarrange(c1, c2, ncol = 2, nrow = 1, align = "v", widths = c(0.5, 0.5))
 # fig.1a 
-ggsave(plot = fig.1a, filename = paste0(outpath, "/output/Covariates_GRF.png"), w = 21.5, h = 18, units = "cm", dpi = 400, device = "png")
+# ggsave(plot = fig.1a, filename = paste0(outpath, "/output/Covariates_GRF.png"), w = 21.5, h = 18, units = "cm", dpi = 400, device = "png")
 
 
 # FIGURE 1B. --------------------------------------------------------------
@@ -148,7 +147,7 @@ ggsave(plot = fig.1a, filename = paste0(outpath, "/output/Covariates_GRF.png"), 
 
 # Set Parameters Data Generation ----------------------------------------------------------
 
-beta0 <- 5 # Intercept
+beta0 <- 6 # Intercept
 beta1 <- 0.5 # Coefficient for cov 1
 beta2 <- 0.1 # Coefficient for cov 2
 # beta3 <- 5 # Coefficient for cov 1*2 interaction
@@ -257,14 +256,14 @@ points_df <- as.data.frame(cbind(lg.s$x, lg.s$y)[, 1:2]) %>%
 #   geom_point(data=points_df, aes(x=x, y=y), color='black', size=1.5) 
 
 # Save the plot
-ggsave(paste0(outpath, "/output/True_log_intensity.png"), true.int.plot, width = 6, height = 6, units = "in")
+# ggsave(paste0(outpath, "/output/True_log_intensity.png"), true.int.plot, width = 6, height = 6, units = "in")
 
 # FIGURE 1C. ---------------------------------------------------------------
 
-rast_cellsA <- c(200, 200)
+rast_cellsA <- c(100, 100)
 rast_sizeA <- c(rast_cellsA[1]*bau_east_step, rast_cellsA[2]*bau_north_step)
 # Set size of grid (number of cells) for Site B (Target)
-rast_cellsB <- c(200, 200)
+rast_cellsB <- c(100, 100)
 rast_sizeB <- c(rast_cellsB[1]*bau_east_step, rast_cellsB[2]*bau_north_step)
 
 # Get coords of overall grid domain boundary
@@ -388,13 +387,14 @@ Fig.1c <- true_log_int.rast %>%
   as.data.frame(xy = TRUE) %>% 
   ggplot() + 
   geom_tile(aes(x = x, y = y, fill = int)) + 
-  scale_fill_viridis() +
+  scale_fill_viridis(guide = guide_colorbar(barwidth = 0.5)) +
   coord_fixed() + 
   theme_bw() + 
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         legend.ticks = element_blank(),
-        legend.title = element_blank()) +
+        legend.title = element_blank(),
+        plot.title = element_text(hjust = 0.5)) +
   geom_rect(data = df_extents, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, color = color), 
             fill = NA, linetype = "solid", linewidth = 1) +
   scale_color_identity() +
@@ -403,12 +403,193 @@ Fig.1c <- true_log_int.rast %>%
   ggtitle('True log intensity')
 
 
-# ARRANGE PLOT ------------------------------------------------------------
-
-Fig.1 <- ggarrange(Fig.1a, Fig.1c, ncol = 1, nrow = 2)
-ggsave(paste0(outpath, "/output/Fig1.png"), Fig.1, width = 21.5, height = 18, units = "cm", dpi = 400, device = "png")
-
-
 # FIGURE 1D. -----------------------------------------------------------------
 
+# Crop true intensity to Site A andb B
+true_log_int.rast.SiteA <- crop(true_log_int.rast, ext(rand.gridA))
+true_log_int.rast.SiteB <- crop(true_log_int.rast, ext(rand.gridB))
+
+spp_process <- cbind(x = lg.s$x, y = lg.s$y)
+
+# po <- thinpp[, c("x", "y")]
+
+# For random covariate case
+po <- spp_process
+
+# Trim po to only include points in Site A or B
+po.rand.gridA <- po[
+  po[,1] >= xmin(ext(rand.gridA)) & po[,1] <= xmax(ext(rand.gridA)) & 
+    po[,2] >= ymin(ext(rand.gridA)) & po[,2] <= ymax(ext(rand.gridA)), 
+]
+
+po.rand.gridB <- po[
+  po[,1] >= xmin(ext(rand.gridB)) & po[,1] <= xmax(ext(rand.gridB)) & 
+    po[,2] >= ymin(ext(rand.gridB)) & po[,2] <= ymax(ext(rand.gridB)), 
+]
+
+## ADDING IN RANDOM PRESENCE-ABSENCE GRID
+
+# Set size of grid (number of cells) for PA grid in Site A (Reference)
+# NOTE - must be smaller than total cell number in x y directions
+rast_cellsA <- c(50, 30)
+rast_sizeA <- c(rast_cellsA[1]*res(rand.gridA)[1], rast_cellsA[2]*res(rand.gridA)[2])
+
+# Get coords of overall grid domain boundary
+xmin <- xmin(rand.gridA)
+xmax <- xmax(rand.gridA)
+ymin <- ymin(rand.gridA)
+ymax <- ymax(rand.gridA)
+
+eastingsSITE <- crds(rand.gridA)[,1]
+northingsSITE <- crds(rand.gridA)[,2]
+
+# Set the limit for x and y coord so box is completely inside the domain
+rand.limA <- c(xmax - rast_sizeA[1], ymax - rast_sizeA[2])
+
+# Create random coordinate index for (bottom left?) corner of subgrid within grid domain
+# Do this by generating a random number and finding the nearest eastings/northings value
+# Then use this index on x0 to get the coordinate
+xmin.randA <- eastingsSITE[which.min(abs(eastingsSITE - runif(1, min = xmin, max = rand.limA[1])))]
+ymin.randA <- northingsSITE[which.min(abs(northingsSITE - runif(1, min = ymin, max = rand.limA[2])))]
+
+xmax.randA <- eastingsSITE[which.min(abs(eastingsSITE - (xmin.randA + rast_sizeA[1])))]
+ymax.randA <- northingsSITE[which.min(abs(northingsSITE - (ymin.randA + rast_sizeA[2])))]
+
+PA.rand.gridA <- rast(xmin = xmin.randA, 
+                      xmax = xmax.randA, 
+                      ymin = ymin.randA, 
+                      ymax = ymax.randA, 
+                      nrows = rast_cellsA[1], 
+                      ncols = rast_cellsA[2],
+                      vals = 1:rast_cellsA[2]) # Just setting values for plotting and for converting to a dataframe
+
+crs(PA.rand.gridA) <- "epsg:3857"
+
+# Get extent for plotting
+extPA <- ext(PA.rand.gridA) 
+
+df_extPA <- data.frame(
+  xmin = extPA[1],
+  xmax = extPA[2],
+  ymin = extPA[3],
+  ymax = extPA[4],
+  color = "black",
+  label = "PA Grid",
+  label_x = (extPA[1] + extPA[2]) / 2,  # Center of the extent for text placement
+  label_y = extPA[4] + (extPA[4] - extPA[3]) * 0.1  # Slightly above the top of the extent
+)
+
+#### NOW DO PA SAMPLING ####
+
+
+# Get the domain of region a
+dom_a_bbox <- c(east_min = xmin(PA.rand.gridA), east_max = xmax(PA.rand.gridA), north_min = ymin(PA.rand.gridA), north_max = ymax(PA.rand.gridA))
+
+# Choose a grid size number of rows (for PA sampling)
+PA_a_res <- 10
+dom_a_resE <- (dom_a_bbox["east_max"] - dom_a_bbox["east_min"]) / PA_a_res
+dom_a_resN <- (dom_a_bbox["north_max"] - dom_a_bbox["north_min"]) / PA_a_res
+
+
+# Set centroids of PA sampling grids
+east_seq <- seq(dom_a_bbox["east_min"] + dom_a_resE/2, 
+                dom_a_bbox["east_max"] - dom_a_resE/2, 
+                by = dom_a_resE)
+north_seq <- seq(dom_a_bbox["north_min"] + dom_a_resN/2, 
+                 dom_a_bbox["north_max"] - dom_a_resN/2, 
+                 by = dom_a_resN)
+
+
+# Create a blank PA dataset at Site A (all zeros), located on grids cells defined by random grid domain and our PA sampling grid size
+grid_a <- expand.grid(east_seq, north_seq)
+pa_a <- cbind(grid_a, 0)
+colnames(pa_a) <- c("x", "y", "presence")
+pa_a <- terra::rast(pa_a)
+
+# find species coordinates from underlying LGCP IN GRID A that are in region a
+inbox_idx_a <- which(po.rand.gridA[, "x"] >= dom_a_bbox["east_min"] &
+                       po.rand.gridA[, "x"] <= dom_a_bbox["east_max"] &
+                       po.rand.gridA[, "y"] >= dom_a_bbox["north_min"] &
+                       po.rand.gridA[, "y"] <= dom_a_bbox["north_max"])
+
+
+po_a <- po.rand.gridA[inbox_idx_a, ]
+
+
+# If there's only one PO location, have to adjust so that it formats into dataframe correctly
+if(length(po_a) == 2) {
+  po_a_df <- data.frame(x = po_a[[1]], y = po_a[[2]])
+  
+} else {
+  
+  po_a_df <- as.data.frame(po_a)
+  
+}
+
+
+# Now assuming perfect detection
+po_a_df$presence <- 1
+
+# Get cell indices of the species coordinates
+cell_idx <- terra::cellFromXY(pa_a, po_a_df[, c("x", "y")])
+
+# Fill in the raster with 1 from the cell indices
+pres_idx <- as.numeric(names(table(cell_idx)))
+pa_a[pres_idx] <- 1
+
+
+# pa - region a
+pa_a_df <- as.data.frame(pa_a, xy = TRUE) %>% 
+  mutate(area = dom_a_resE * dom_a_resN) 
+
+### NOW PLOT
+
+# Plot PO data on grid A and B
+Fig.1d <- true_log_int.rast.SiteA %>% 
+  as.data.frame(xy = T) %>% 
+  ggplot() +
+  geom_tile(aes(x = x, y = y, fill = int)) +
+  scale_fill_viridis(guide = guide_colorbar(barwidth = 0.5)) +
+  coord_fixed() +
+  geom_rect(data = df_extPA, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, color = color), 
+            fill = NA, linetype = "solid", linewidth = 1) +
+  geom_point(data = po.rand.gridA, aes(x = x, y = y), color = "red", size = 0.8) +
+  geom_point(data = pa_a_df, aes(x=x, y=y, shape = factor(presence, levels = c(1,0))), size = 0.9) +
+  scale_shape_manual(values = c("0" = 4, "1" = 16),
+                     label = c("0" = "Absence", "1" = "Presence")) +
+  scale_color_identity() +
+  theme_bw() +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.ticks = element_blank(),
+        legend.title = element_blank(),
+        plot.margin = unit(c(0.5, 0.1, 0.5, 0.1), "lines"),
+        plot.title = element_text(hjust = 0.5)) +  # Reduce margins
+  ggtitle('Site A')
+
+Fig.1e <- true_log_int.rast.SiteB %>% 
+  as.data.frame(xy = T) %>% 
+  ggplot() +
+  geom_tile(aes(x = x, y = y, fill = int)) +
+  scale_fill_viridis(guide = guide_colorbar(barwidth = 0.5)) +
+  coord_fixed() +
+  geom_point(data = po.rand.gridB, aes(x = x, y = y), color = "red", size = 0.8) +
+  theme_bw() +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        legend.ticks = element_blank(),
+        legend.title = element_blank(),
+        plot.margin = unit(c(0.5, 0.1, 0.5, 0.1), "lines"),
+        plot.title = element_text(hjust = 0.5)) +  # Reduce margins
+  ggtitle('Site B')
+
+Fig.1de <- ggarrange(Fig.1d, Fig.1e, ncol = 2, nrow = 1, align = "v", widths = c(0.5, 0.5))
+
+
+
+# ARRANGE PLOT ------------------------------------------------------------
+
+Fig.1 <- ggarrange(Fig.1a, Fig.1c, Fig.1de,  ncol = 1, nrow = 3, heights = c(0.4, 0.4, 0.4), labels = c("(a)", "(b)", "(c)"), label.x = 0)
+
+ggsave(paste0(outpath, "/output/Fig1.png"), Fig.1, width = 21, height = 28, units = "cm", dpi = 400, device = "png")
 
