c486a00f2ef5e63806c6404c9d2ea854387c7c32
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-09-02 11:17:47 +1000

    12. Plot validation true intensity

---
+++ 11.Plot_Validation_True_Intensity.R
@@ -0,0 +1,70 @@
+
+library(ggpubr)
+
+# 11. Plot Validation True Intensity --------------------------------------
+
+plot_validation_func <- function(true.validation.df) {
+  
+  # Plot the validation
+  
+  cor <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = correlation, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "Correlation", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  MAE <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = MAE, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "MAE", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  RMSE <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = RMSE, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "RMSE", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  Int.score.mean <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = Mean.Int.Score, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "Mean Interval Score", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  Int.score.sum <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = Sum.Int.Score, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "Sum Interval Score", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  p1 <- ggarrange(RMSE, MAE, common.legend = T,  ncol = 2, nrow = 1)
+  
+  p2 <- ggarrange(Int.score.mean, Int.score.sum, common.legend = T,  ncol = 2, nrow = 1)
+  
+  print(p1) 
+  
+  print(p2) 
+  
+  print(cor)
+  
+  ## Smaller values of interval score are better
+  
+  # ggsave(plot = p1, filename = paste0("output/Extrap_10_rep_PO_ISDM_RMSE_MAE_plot.png"), w = 21.5, h = 15, units = "cm", dpi = 400, device = "png")
+  # 
+  # ggsave(plot = p2, filename = paste0("output/Extrap_10_rep_PO_ISDM_Int_Score_plot.png"), w = 21.5, h = 15, units = "cm", dpi = 400, device = "png")
+  # 
+  # ggsave(plot = cor, filename = paste0("output/Extrap_10_rep_PO_ISDM_COR_plot.png"), w = 21.5, h = 15, units = "cm", dpi = 400, device = "png")
+  # 
+
+  
+}
