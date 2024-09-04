e3d4740e9c97dd390684f7d86f08a94fae12c6b4
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-09-05 08:27:38 +1000

    Save model output plots

--- 12.Plot_Model_Outputs.R
+++ 12.Plot_Model_Outputs.R
@@ -1,6 +1,10 @@
 
 plot_residuals_func <- function(reps.setup.list, 
-                                output.path) {
+                                outpath,
+                                scenario_name) {
+  
+  # Get the names of the extrap types for indexing
+  extrap_names <- names(reps.setup.list)
   
   for(extrap.type in seq_along(reps.setup.list)) {
     
@@ -10,14 +14,17 @@ plot_residuals_func <- function(reps.setup.list,
     # For every replicate
     for(rep in seq_along(reps.setup.list[[name]])) {
       
+      # Get path for saving
+      rep_path <- file.path(outpath, scenario_name, name, paste0("Rep_", rep))
+      
       # Extract the models dataframe [[name]] double brackets for list extract
       models_df <- reps.setup.list[[name]][[rep]]$models
       
       for (i in seq_along(models_df)) {
         
-        png(paste0(output.path, "/plot.isdm_EXTRAP_", extrap_names[extrap.type], "_", as.character(models_df[i, "Mod.type"]),  ".png"), width = 10, height = 10, units = "in", res = 300)
+        png(paste0(rep_path, "/plot.isdm_EXTRAP_", extrap_names[extrap.type], "_", as.character(models_df[i, "Mod.type"]),  ".png"), width = 10, height = 10, units = "in", res = 300)
         
-        plot(models_df[[i, "Model"]], nFigRow = 2, ask = FALSE)
+        plot(models_df[[i, 'Model']], nFigRow = 2, ask = FALSE)
         
         dev.off()
         
@@ -28,3 +35,4 @@ plot_residuals_func <- function(reps.setup.list,
   }
   
 }
+
