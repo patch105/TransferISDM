7353a07e06279388489ad0e6e6b969293ba7406a
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-09-08 11:21:42 +1000

    Predict and validate for Site A

--- 10.Validation_True_Intensity.R
+++ 10.Validation_True_Intensity.R
@@ -4,7 +4,7 @@ library(scoringutils)
 
 # 10. Validation true intensity -------------------------------------------
 
-validation_func <- function(reps.setup.list) {
+validation_SiteB_func <- function(reps.setup.list) {
   
   # Create an empty list to store results
   results_list <- list()
@@ -85,3 +85,90 @@ validation_func <- function(reps.setup.list) {
   return(true.validation.df)
   
 }
+
+
+# OPTIONAL - RUN THE VALIDATION FOR SITE A --------------------------------
+
+
+validation_SiteA_func <- function(reps.setup.list) {
+  
+  # Create an empty list to store results
+  results_list <- list()
+  
+  # Get the names of the extrap types for indexing
+  extrap_names <- names(reps.setup.list)
+  
+  # For every level of extrap type (Low, Mod, High)
+  for(extrap.type in seq_along(reps.setup.list)) {
+    
+    # Extract the name ("Low") for indexing from the names list
+    name <- extrap_names[extrap.type] 
+    
+    # For every replicate
+    for(rep in seq_along(reps.setup.list[[name]])) {
+      
+      # Extract the models dataframe [[name]] double brackets for list extract
+      models_df <- reps.setup.list[[name]][[rep]]$models
+      
+      # Crop out the true log intensity from the Site A
+      rand.gridA <- reps.setup.list[[name]][[rep]]$extrap.reps.out$rand.gridA
+      true_log_int.rast <- reps.setup.list[[name]][[rep]]$true_log_int.rast
+      true_log_int.rast.SiteA <- crop(true_log_int.rast, ext(rand.gridA))
+      
+      for (i in seq_along(models_df)) { # NEED TO ADD BACK IN ONCE HAVE PA WORKING
+        
+        mod <- models_df[[i, "Model"]]
+        
+        # Pull out the mean intensity prediction for each cell
+        mean.int.pred <- mod[[1]]$preds.link.siteA$field$Mean
+        
+        # Pull out the lower and upper bounds of the prediction
+        lower.int.pred <- mod[[1]]$preds.link.siteA$field$Lower
+        
+        upper.int.pred <- mod[[1]]$preds.link.siteA$field$Upper
+        
+        # Metrics from Simmonds et al. 
+        # Compare the predicted intensity to the true intensity 
+        cor <- cor(as.vector(mean.int.pred), as.vector(true_log_int.rast.SiteA))
+        
+        MAE <- mean(abs(as.vector(mean.int.pred - true_log_int.rast.SiteA)))
+        
+        RMSE <- Metrics::rmse(actual = as.vector(true_log_int.rast.SiteA), 
+                              predicted = as.vector(mean.int.pred))
+        
+        ### Calculating the Interval Score ###
+        
+        interval_score <- interval_score(true_values = as.vector(true_log_int.rast.SiteA),
+                                         lower = as.vector(lower.int.pred), 
+                                         upper = as.vector(upper.int.pred),
+                                         interval_range = 95,
+                                         weigh = TRUE)
+        
+        Sum.Int.Score <- sum(interval_score)
+        
+        Mean.Int.Score <- mean(interval_score)
+        
+        # Save results to list
+        
+        results_list[[length(results_list) + 1]] <- data.frame(
+          extrap.type = name,
+          rep = rep,
+          mod.type = as.character(models_df[i, "Mod.type"]),
+          correlation = cor,
+          MAE = MAE,
+          RMSE = RMSE,
+          Sum.Int.Score = Sum.Int.Score,
+          Mean.Int.Score = Mean.Int.Score
+        )
+        
+      }}
+    
+  }
+  
+  # Combine all the results into a single dataframe
+  true.validation.df <- do.call(rbind, results_list)
+  
+  return(true.validation.df)
+  
+}
+
--- 11.Plot_Validation_True_Intensity.R
+++ 11.Plot_Validation_True_Intensity.R
@@ -3,10 +3,10 @@ library(ggpubr)
 
 # 11. Plot Validation True Intensity --------------------------------------
 
-plot_validation_func <- function(true.validation.df,
-                                 save = FALSE,
-                                 outpath,
-                                 scenario_name) {
+plot_validation_SiteB_func <- function(true.validation.df,
+                                       save = FALSE,
+                                       outpath,
+                                       scenario_name) {
   
   # Plot the validation
   
@@ -73,3 +73,81 @@ plot_validation_func <- function(true.validation.df,
   
   
 }
+
+
+# OPTIONAL - PLOT VALIDATION SITE A ---------------------------------------
+
+plot_validation_SiteA_func <- function(true.validation.df,
+                                       save = FALSE,
+                                       outpath,
+                                       scenario_name) {
+  
+  # Plot the validation
+  
+  cor <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = correlation, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "Correlation", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  MAE <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = MAE, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "MAE", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  RMSE <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = RMSE, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "RMSE", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  ## Smaller values of interval score are better
+  
+  Int.score.mean <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = Mean.Int.Score, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "Mean Interval Score", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  Int.score.sum <- true.validation.df %>% 
+    ggplot(aes(x = extrap.type, y = Sum.Int.Score, fill = mod.type)) +
+    geom_boxplot() +
+    labs(x = "Extrapolation", y = "Sum Interval Score", fill = "Model Type") +
+    scale_x_discrete(labels = c("Low", "Mod", "High")) +
+    scale_fill_manual(values = c("Integrated.no.GRF" = "purple", "PO.no.GRF" = "skyblue", "PA.no.GRF" = "orange")) +
+    theme_bw()
+  
+  p1 <- ggarrange(RMSE, MAE, common.legend = T,  ncol = 2, nrow = 1)
+  
+  p2 <- ggarrange(Int.score.mean, Int.score.sum, common.legend = T,  ncol = 2, nrow = 1)
+  
+  if(save == TRUE) {
+    
+    # Make directory for Site A
+    dir.create(file.path(outpath, scenario_name, "SiteA"), showWarnings = FALSE)
+    
+    ggsave(plot = p1, filename = paste0(file.path(outpath, scenario_name, "SiteA"),"/Scenario_", scenario_name, "_RMSE_MAE_plot.png"), w = 21.5, h = 15, units = "cm", dpi = 400, device = "png")
+    
+    ggsave(plot = p2, filename = paste0(file.path(outpath, scenario_name, "SiteA"),"/Scenario_", scenario_name, "_Int_Score_plot.png"), w = 21.5, h = 15, units = "cm", dpi = 400, device = "png")
+    
+    ggsave(plot = cor, filename = paste0(file.path(outpath, scenario_name, "SiteA"),"/Scenario_", scenario_name, "_COR_plot.png"), w = 21.5, h = 15, units = "cm", dpi = 400, device = "png")
+    
+    
+  } else{print(p1) 
+    
+    print(p2) 
+    
+    print(cor)}
+  
+  
+}
+
