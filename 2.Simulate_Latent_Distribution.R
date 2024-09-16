86dda8e9b615cbeae6eba6955995e8f64389bfad
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-09-17 08:32:20 +1000

    Simulate split latent distribution (fixed and random effects)

--- 2.Simulate_Latent_Distribution.R
+++ 2.Simulate_Latent_Distribution.R
@@ -75,43 +75,6 @@ sim_latent_dist_func <- function(beta0,
 }
 
 
-
-# # IDEAS ON HOW TO SPLIT LATENT & COVARIATE EFFECTS ------------------------
-# 
-# GRF <- nlm_gaussianfield(ncol = ncol,
-#                          nrow = nrow,
-#                          resolution = res,
-#                          autocorr_range = scal, # Maximum range (raster units) of spatial autocorrelation
-#                          mag_var = variance, # Magnitude of variation over the landscape
-#                          nug = 0.01, # Magnitude of variation in the scale of the autocorr_range (smaller values = more homogenous)
-#                          mean = 0, # Mean value over the field
-#                          user_seed = NULL, # Set random seed for the simulation
-#                          rescale = F # If T, the values are rescaled between 0 and 1
-# ) %>% 
-#   rast()
-# 
-# 
-# # Note to self FOR SIMULATING LATENT STATE NEXT - the im function requires a matrix of a certain input order
-# # Terra::as.matrix just converts all cells to a matrix in the same order as they appear in the raster
-# 
-# # The code below therefore: converts the raster to a matrix, retaining the terra ordering
-# # We then reshape into long (x, y) format 
-# # We extract the coords from the original raster and bind to the covariate matrix
-# 
-# # # Get coords of original raster
-# # coords <- xyFromCell(GRF, 1:ncell(GRF))
-# 
-# # Convert raster to matrix object
-# GRF.df <- as.data.frame(GRF, xy = T)
-# GRF.mat <- as.matrix(GRF.df)
-# colnames(GRF.mat) <- c("x", "y", "GRF")
-# 
-# mu <- mu + exp(GRF.mat[, "GRF"])
-# 
-# lg.s <- rpoispp(mu)
-
-
-
 # TO DOs ------------------------------------------------------------------
 
 # How does virtual species code in a function for the mean??
