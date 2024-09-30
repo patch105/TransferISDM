7ff20426be1923db1abcbeae400597e3b9f4b74c
Charlotte Ruby Patterson <n11222026@qut.edu.au>
2024-10-01 09:12:38 +1000

    Adding in plotting of sites on covariates

--- 12B.Plot_Data.R
+++ 12B.Plot_Data.R
@@ -27,6 +27,9 @@ plot_data_func <- function(reps.setup.list,
       rand.gridB <- reps.setup.list[[name]][[rep]]$extrap.reps.out$rand.gridB
       true_log_int.rast.SiteB <- crop(true_log_int.rast, ext(rand.gridB))
       
+      cov1 <- reps.setup.list[[name]][[rep]]$cov.list$cov1
+      cov2 <- reps.setup.list[[name]][[rep]]$cov.list$cov2
+      
       # Get extent for plotting
       extPA <- ext(reps.setup.list[[name]][[rep]]$PA.rand.gridA) 
       
@@ -80,6 +83,77 @@ plot_data_func <- function(reps.setup.list,
               plot.title = element_text(hjust = 0.5)) +  # Reduce margins
         ggtitle('Site B')
       
+      # Extract the extents
+      extA <- ext(rand.gridA)
+      extB <- ext(rand.gridB)
+      
+      df_extA <- data.frame(
+        xmin = extA[1],
+        xmax = extA[2],
+        ymin = extA[3],
+        ymax = extA[4],
+        color = "red",
+        label = "Reference Site",
+        label_x = (extA[1] + extA[2]) / 2,  # Center of the extent for text placement
+        label_y = extA[4] + (extA[4] - extA[3]) * 0.1  # Slightly above the top of the extent
+      )
+      
+      df_extB <- data.frame(
+        xmin = extB[1],
+        xmax = extB[2],
+        ymin = extB[3],
+        ymax = extB[4],
+        color = "blue",
+        label = "Target Site",
+        label_x = (extB[1] + extB[2]) / 2,  # Center of the extent for text placement
+        label_y = extB[4] + (extB[4] - extB[3]) * 0.1  # Slightly above the top of the extent
+      )
+      
+      # Combine the extents into one data frame
+      df_extents <- rbind(df_extA, df_extB)
+      
+      # Create ggplot
+      C <- cov1 %>% 
+        as.data.frame(xy = TRUE) %>% 
+        ggplot() + 
+        geom_tile(aes(x = x, y = y, fill = cov)) + 
+        scale_fill_viridis(guide = guide_colorbar(barwidth = 0.5)) +
+        coord_fixed() + 
+        theme_bw() + 
+        theme(axis.title.x = element_blank(),
+              axis.title.y = element_blank(),
+              legend.ticks = element_blank(),
+              legend.title = element_blank(),
+              plot.title = element_text(hjust = 0.5)) +
+        geom_rect(data = df_extents, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, color = color), 
+                  fill = NA, linetype = "solid", linewidth = 1) +
+        scale_color_identity() +
+        geom_text(data = df_extents, aes(x = label_x, y = label_y, label = label), 
+                  color = "black", size = 5, vjust = 0) +
+        ggtitle('Covariate 1')
+      
+      # Create ggplot
+      D <- cov2 %>% 
+        as.data.frame(xy = TRUE) %>% 
+        ggplot() + 
+        geom_tile(aes(x = x, y = y, fill = cov)) + 
+        scale_fill_viridis(guide = guide_colorbar(barwidth = 0.5)) +
+        coord_fixed() + 
+        theme_bw() + 
+        theme(axis.title.x = element_blank(),
+              axis.title.y = element_blank(),
+              legend.ticks = element_blank(),
+              legend.title = element_blank(),
+              plot.title = element_text(hjust = 0.5)) +
+        geom_rect(data = df_extents, aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, color = color), 
+                  fill = NA, linetype = "solid", linewidth = 1) +
+        scale_color_identity() +
+        geom_text(data = df_extents, aes(x = label_x, y = label_y, label = label), 
+                  color = "black", size = 5, vjust = 0) +
+        ggtitle('Covariate 1')
+     
+      E <- ggarrange(C, D, ncol = 1, nrow = 2)
+      
       # Save plots
       rep_path <- file.path(outpath, scenario_name, name, paste0("Rep_", rep, "Job_", job_index))
       
@@ -93,7 +167,7 @@ plot_data_func <- function(reps.setup.list,
       
       ggsave(paste0(rep_path, "/SiteA_PO_PA_Plot.png"), A, width = 21, height = 25, units = "cm", dpi = 400, device = "png")
       ggsave(paste0(rep_path, "/SiteB_Plot.png"), B, width = 21, height = 25, units = "cm", dpi = 400, device = "png")
-      
+      ggsave(paste0(rep_path, "/Sites_on_Covs_Plot.png"), E, width = 21, height = 25, units = "cm", dpi = 400, device = "png")
       
     }
     
